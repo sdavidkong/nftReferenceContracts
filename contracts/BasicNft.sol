7f26a0e7cb834a5c38f23394de718d047e20a462
S David Kong <SDAVIDKONG@gmail.com>
2022-10-25 18:58:55 -0400

    contract update

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.7;
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol"
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNFT {}
+contract BasicNFT is ERC721 {
+    constructor() ERC721("Athena", "OE1")
+}
