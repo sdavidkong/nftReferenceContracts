cf85025af97b10822b4646a59184867b028242dd
S David Kong <SDAVIDKONG@gmail.com>
2022-10-25 18:05:46 -0400

    updated contract 1

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.7;
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
-contract Lock {
-
-}
+contract BasicNFT {}
