dfefffc4954ce7e2488047393cbcce26f8e5274e
S David Kong <SDAVIDKONG@gmail.com>
2022-10-25 18:44:00 -0400

    contract update

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"
 
 contract BasicNFT {}
