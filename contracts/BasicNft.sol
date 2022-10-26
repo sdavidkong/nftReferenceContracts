18b68e8e91e043e36b90a0106b14f32cb2b70ce3
S David Kong <SDAVIDKONG@gmail.com>
2022-10-26 00:13:46 -0400

    updated contract

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -6,5 +6,17 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
-    constructor() ERC721("Athena", "OE1")
+    uint256 private s_tokenCounter;
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    constructor() ERC721("Athena", "OE1") {}
+
+    function mintNft() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
