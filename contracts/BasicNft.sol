54edf21beb2f3d415123fe38edc1dc6dbdd2c2cb
S David Kong <SDAVIDKONG@gmail.com>
2022-10-26 21:43:50 -0400

    added deploy script

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -14,6 +14,14 @@ contract BasicNFT is ERC721 {
 
     function mintNft() public returns (uint256) {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+        return s_tokenCounter;
+    }
+
+    function tokenURI(
+        uint256 /*tokenId*/
+    ) public view override returns (string memory) {
+        return TOKEN_URI;
     }
 
     function getTokenCounter() public view returns (uint256) {
